432376409030c5473554b491978fe6a617f62e3d
Towhidul123 <excaliburfsh@gmail.com>
2023-09-10 12:36:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import Counter from './Counter';
+import Team from './Team';
+import Users from '../Users';
+
+function App() {
+
+  
+ 
+  function handleClick(){
+    alert('button clicked');
+  }
+
+  const handleClick2 =() =>{
+    alert("Button 2 Clicked");
+  }
+
+  const addToFive =(num) =>{
+    alert(num +5);
+  }
+
+  return (
+    <>
+    
+      <h3>React Core Concepts 2</h3>
+      <Users></Users>
+      <Team></Team>
+
+     <Counter></Counter>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={() => {alert('third clicked')}}>third</button>
+      {/* vajaila */}
+      <button onClick={() =>addToFive(3)}>Four</button>
+    </>
+  )
+}
+
+export default App
